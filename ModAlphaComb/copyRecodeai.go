ad0ed467d55c0a85af8dcbe5a954a3046fb5afa6
jobiabo <joelobiabo@gmail.com>
2026-03-08 15:02:54 +0000

    refixed text to replace

--- ModAlphaComb/copyRecodeai.go
+++ ModAlphaComb/copyRecodeai.go
@@ -9,6 +9,7 @@ import (
 func CapToUpperMod() {
 
 	text := "it (cap) was the best of times, it was the worst of times (up) , it was the age of wisdom, it was the age of foolishness (cap, 6) , it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of darkness, it was the spring of hope, IT WAS THE (low, 3) winter of despair."
+	text = strings.ReplaceAll(text, ", ", ",")
 	textSplit := strings.Fields(text)
 
 	for i := 0; i < len(textSplit); i++ {
